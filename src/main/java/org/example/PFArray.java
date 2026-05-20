a33e66996a86887a67b35dd8517f0011258f04a7
HoangTiepLukas <hoang.vantiep@1pf.cz>
2026-05-20 10:25:30 +0200

    design PFArray class

--- src/main/java/org/example/PFArray.java
+++ src/main/java/org/example/PFArray.java
@@ -1,5 +1,31 @@
 package org.example;
 
 public class PFArray {
+    private int[] data;
 
+    public PFArray() {
+        this.data = null;
+    }
+
+    public void createRandomArray() {}
+
+    public void createManualArray(String input) {}
+
+    public void printArray() {}
+
+    public int[] getArray() { return data; }
+
+    public void addNumber(int number) {}
+
+    public int findMax() { return 0; }
+
+    public int findMin() { return 0; }
+
+    public int sum() { return 0; }
+
+    public void removeNumber(int number) {}
+
+    public void deleteArray() { data = null; }
+
+    public void generateNumber() {}
 }
